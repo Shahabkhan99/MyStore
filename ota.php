c15001ada18d2970b3a97c34a308cfe736547bdc
Shahab Khan <37383411+Shahabkhan99@users.noreply.github.com>
2019-07-16 20:18:57 +0300

    Create ota.php

---
+++ ota.php
@@ -0,0 +1,12 @@
+{"notification":[{"id":"311","title":"Coba dl","body":"
+Coba lagi<\/p>\r\n","date":"2019-07-12 17:01:01","status_read":"unread"},{"id":"310","title":"Second memo","body":"
+
+Gas bos<\/p>\r\n","date":"2019-07-12 16:59:18","status_read":"unread"},{"id":"309","title":"99jitu first memo","body":"
+
+Bukancalak bos<\/p>\r\n","date":"2019-07-12 16:58:12","status_read":"unread"},{"id":"306","title":"salam jackpot 2 jari","body":"
+
+selamat kepada pemenang periode xxx. jackpot 4d pasaran singapore 1st prize. berapapun kemenangan anda pasti di bayar. 99jitu corporation<\/p>\r\n","date":"2019-07-10 10:46:05","status_read":"unread"},{"id":"305","title":"Welcome 99jitu","body":"
+
+Selamat datang di 99jitu.com selamat bermain dan semoga beruntung<\/p>\r\n","date":"2019-07-10 10:43:56","status_read":"unread"},{"id":"303","title":"Pengumuman Pertama push notif","body":"
+
+Selamat Malam semua saat ini kita sedang mencoba notif baru<\/p>\r\n","date":"2019-07-10 10:45:43","status_read":"unread"}],"success":true,"message":" berhasil"}
