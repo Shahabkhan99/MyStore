d2323af1056d60b80842b7ac2639a31e613ae30f
Shahab Khan <37383411+Shahabkhan99@users.noreply.github.com>
2019-07-16 20:29:58 +0300

    Update ota.php

--- ota.php
+++ ota.php
@@ -1,12 +1,19 @@
-{"notification":[{"id":"311","title":"Coba dl","body":"
-1first Message<\/p>\r\n","date":"2019-07-12 17:01:01","status_read":"unread"},{"id":"310","title":"100 memo","body":"
+{"notification":[
 
-2first Message<\/p>\r\n","date":"2019-07-12 16:59:18","status_read":"unread"},{"id":"309","title":"200 memo","body":"
+{"id":"311","title":"100 memo","body":"
+1first Message<\/p>\r\n","date":"2019-07-12 17:01:01","status_read":"unread"},
 
-3first Message<\/p>\r\n","date":"2019-07-12 16:58:12","status_read":"unread"},{"id":"306","title":"300 memo","body":"
+{"id":"310","title":"200 memo","body":"
+2first Message<\/p>\r\n","date":"2019-07-12 16:59:18","status_read":"unread"},
 
-4first Message<\/p>\r\n","date":"2019-07-10 10:46:05","status_read":"unread"},{"id":"305","title":"400 memo","body":"
+{"id":"309","title":"300 memo","body":"
+3first Message<\/p>\r\n","date":"2019-07-12 16:58:12","status_read":"unread"},
 
-5first Message<\/p>\r\n","date":"2019-07-10 10:43:56","status_read":"unread"},{"id":"303","title":"500 memo","body":"
+{"id":"306","title":"400 memo","body":"
+4first Message<\/p>\r\n","date":"2019-07-10 10:46:05","status_read":"unread"},
 
+{"id":"305","title":"500 memo","body":"
+5first Message<\/p>\r\n","date":"2019-07-10 10:43:56","status_read":"unread"},
+
+{"id":"303","title":"600 memo","body":"
 6first Message<\/p>\r\n","date":"2019-07-10 10:45:43","status_read":"unread"}],"success":true,"message":" berhasil"}
