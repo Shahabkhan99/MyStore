6475ed69d02770f30ce9522804ec3e984b5266f0
Shahab Khan <37383411+Shahabkhan99@users.noreply.github.com>
2019-07-16 20:24:26 +0300

    Update ota.php

--- ota.php
+++ ota.php
@@ -1,12 +1,12 @@
 {"notification":[{"id":"311","title":"Coba dl","body":"
-Coba lagi<\/p>\r\n","date":"2019-07-12 17:01:01","status_read":"unread"},{"id":"310","title":"Second memo","body":"
+1first Message<\/p>\r\n","date":"2019-07-12 17:01:01","status_read":"unread"},{"id":"310","title":"100 memo","body":"
 
-Gas bos<\/p>\r\n","date":"2019-07-12 16:59:18","status_read":"unread"},{"id":"309","title":"99jitu first memo","body":"
+2first Message<\/p>\r\n","date":"2019-07-12 16:59:18","status_read":"unread"},{"id":"309","title":"200 memo","body":"
 
-Bukancalak bos<\/p>\r\n","date":"2019-07-12 16:58:12","status_read":"unread"},{"id":"306","title":"salam jackpot 2 jari","body":"
+3first Message<\/p>\r\n","date":"2019-07-12 16:58:12","status_read":"unread"},{"id":"306","title":"300 memo","body":"
 
-selamat kepada pemenang periode xxx. jackpot 4d pasaran singapore 1st prize. berapapun kemenangan anda pasti di bayar. 99jitu corporation<\/p>\r\n","date":"2019-07-10 10:46:05","status_read":"unread"},{"id":"305","title":"Welcome 99jitu","body":"
+4first Message<\/p>\r\n","date":"2019-07-10 10:46:05","status_read":"unread"},{"id":"305","title":"400 memo","body":"
 
-Selamat datang di 99jitu.com selamat bermain dan semoga beruntung<\/p>\r\n","date":"2019-07-10 10:43:56","status_read":"unread"},{"id":"303","title":"Pengumuman Pertama push notif","body":"
+5first Message<\/p>\r\n","date":"2019-07-10 10:43:56","status_read":"unread"},{"id":"303","title":"500 memo","body":"
 
-Selamat Malam semua saat ini kita sedang mencoba notif baru<\/p>\r\n","date":"2019-07-10 10:45:43","status_read":"unread"}],"success":true,"message":" berhasil"}
+6first Message<\/p>\r\n","date":"2019-07-10 10:45:43","status_read":"unread"}],"success":true,"message":" berhasil"}
